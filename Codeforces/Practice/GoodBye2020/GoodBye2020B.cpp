bc17a0c52356f678f9646d2c3692956bfd156ea4
Archan Patel <archan.patel890@gmail.com>
2021-07-26 21:30:02 -0700

    Added Practice Round 695, EduRound103, EduRound111, GoodBye2020

---
+++ Codeforces/Practice/GoodBye2020/GoodBye2020B.cpp
@@ -0,0 +1,168 @@
+/*
+* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
+*       Example:-   $ g++ -std=c++11 c++Template.cpp
+*/
+ 
+/********   All Required Header Files ********/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+// IO
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define read(type) readInt<type>()
+// Array / Iteration
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+// Function Aliases
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define sz(v) int(v.size())
+#define ft first
+#define sc second
+#define mp make_pair
+#define pb push_back
+// Constants
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const int MOD = 1000000007;
+const double pi=acos(-1.0);
+// typedefs aliases for common types
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vc> vvc;
+typedef vector<vpii> vvpii;
+typedef vector<vpll> vvpll;
+typedef map<int,int> mpii;
+typedef map<ll,ll> mpll;
+typedef set<int> seti;
+typedef set<ll> setll;
+typedef multiset<int> mseti;
+typedef multiset<ll> msetll;
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<ll, ll> umapll;
+// Timing
+clock_t start_time = clock();
+#define current_time fixed<<setprecision(6)<<(ld)(clock()-start_time)/CLOCKS_PER_SEC
+
+/****** Input / Output Redirection to Files ******/
+#define file_input freopen("input.txt","r",stdin)
+#define file_output freopen("output.txt","w",stdout)
+#define file_io file_input; file_output
+#define fast_io ios_base::sync_with_stdio(0);cin.tie(0)
+
+/****** Template of some basic operations *****/
+template<typename T, typename U> inline void amin(T &x, U y) { if (y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if (x < y) x = y; }
+/**********************************************/
+
+/****** Template of Fast I/O Methods *********/
+template <typename T> inline void write(T x)
+{
+	int i = 20;
+	char buf[21];
+	// buf[10] = 0;
+	buf[20] = '\n';
+
+	do
+	{
+		buf[--i] = x % 10 + '0';
+		x /= 10;
+	} while(x);
+	do
+	{
+		putchar(buf[i]);
+	} while (buf[i++] != '\n');
+}
+
+template <typename T> inline T readInt()
+{
+	T n = 0, s = 1;
+	char p = getchar();
+	if (p == '-')
+		s = -1;
+	while ((p < '0' || p > '9') && p !=EOF && p != '-')
+		p = getchar();
+	if (p == '-')
+		s = -1 , p = getchar();
+	while (p >= '0' && p <= '9') {
+		n = (n << 3) + (n << 1) + (p - '0');
+		p = getchar();
+	}
+
+	return n*s;
+}
+/************************************/
+
+
+/***************** Debugging ******************/
+#define debug(x) cerr << #x << "=" << (x) <<'\n'
+/**********************************************/
+
+
+/******** User-defined Function *******/
+void solve(vi &x, int n) {
+	int cnt = 0;
+	if (x.size() > 0) ++cnt;
+	for (int i = 1; i < n; ++i) {
+		if (x[i-1] == x[i] && ((i < n-1 && x[i+1] != x[i]) || i == n-1)) x[i]++;
+		if (x[i-1] != x[i]) {
+			++cnt;
+		}
+	}
+
+	cout << cnt << endl;
+}
+
+/**************************************/
+
+
+/********** Main()  function **********/
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	file_input;
+	//file_output;
+	#endif
+
+	int tc;
+	tc = read(int);
+
+	while (tc--) {
+		int n = read(int);
+		vi x(n);
+		for (auto &i : x) {
+			i = read(int);
+		}
+		solve(x, n);
+	}
+	return 0;
+}
+/********  Main() Ends Here *************/
---
+++ Codeforces/Practice/GoodBye2020/GoodBye2020E.cpp
@@ -0,0 +1,225 @@
+/*
+* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
+*       Example:-   $ g++ -std=c++11 c++Template.cpp
+*/
+ 
+/********   All Required Header Files ********/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+// IO
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define read(type) readInt<type>()
+// Array / Iteration
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+// Function Aliases
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define sz(v) int(v.size())
+#define ft first
+#define sc second
+#define mp make_pair
+#define pb push_back
+// Constants
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const int MOD = 1000000007;
+const double pi=acos(-1.0);
+// typedefs aliases for common types
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vc> vvc;
+typedef vector<vpii> vvpii;
+typedef vector<vpll> vvpll;
+typedef map<int,int> mpii;
+typedef map<ll,ll> mpll;
+typedef set<int> seti;
+typedef set<ll> setll;
+typedef multiset<int> mseti;
+typedef multiset<ll> msetll;
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<ll, ll> umapll;
+// Timing
+clock_t start_time = clock();
+#define current_time fixed<<setprecision(6)<<(ld)(clock()-start_time)/CLOCKS_PER_SEC
+
+/****** Input / Output Redirection to Files ******/
+#define file_input freopen("input.txt","r",stdin)
+#define file_output freopen("output.txt","w",stdout)
+#define file_io file_input; file_output
+#define fast_io ios_base::sync_with_stdio(0);cin.tie(0)
+
+/****** Template of some basic operations *****/
+template<typename T, typename U> inline void amin(T &x, U y) { if (y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if (x < y) x = y; }
+/**********************************************/
+
+/****** Template of Fast I/O Methods *********/
+template <typename T> inline void write(T x)
+{
+	int i = 20;
+	char buf[21];
+	// buf[10] = 0;
+	buf[20] = '\n';
+
+	do
+	{
+		buf[--i] = x % 10 + '0';
+		x /= 10;
+	} while(x);
+	do
+	{
+		putchar(buf[i]);
+	} while (buf[i++] != '\n');
+}
+
+template <typename T> inline T readInt()
+{
+	T n = 0, s = 1;
+	char p = getchar();
+	if (p == '-')
+		s = -1;
+	while ((p < '0' || p > '9') && p !=EOF && p != '-')
+		p = getchar();
+	if (p == '-')
+		s = -1 , p = getchar();
+	while (p >= '0' && p <= '9') {
+		n = (n << 3) + (n << 1) + (p - '0');
+		p = getchar();
+	}
+
+	return n*s;
+}
+/************************************/
+
+
+/***************** Debugging ******************/
+#define debug(x) cerr << #x << "=" << (x) <<'\n'
+/**********************************************/
+
+template <int mod>
+struct modint {
+    int value;
+
+    modint(long long x = 0) { value = normalize(x); }
+
+    int normalize(long long x) {
+        if (x < -mod || x >= mod)
+            x %= mod;
+        if (x < 0)
+            x += mod;
+        return static_cast<int>(x);
+    }
+
+    explicit operator int() const { return value; }
+
+    modint operator-() const { return modint(-value); }
+
+    modint &operator+=(modint rhs) {
+        if ((value += rhs.value) >= mod)
+            value -= mod;
+        return *this;
+    }
+
+    modint &operator-=(modint rhs) {
+        if ((value -= rhs.value) < 0)
+            value += mod;
+        return *this;
+    }
+
+    modint &operator*=(modint rhs) {
+        value = normalize(static_cast<long long>(value) * rhs.value);
+        return *this;
+    }
+
+    modint &operator/=(modint rhs) { return *this *= modint(inverse(rhs.value, mod)); }
+
+    int inverse(int a, int m) {
+        int u = 0, v = 1;
+        while (a != 0) {
+            int t = m / a;
+            m -= t * a;
+            swap(a, m);
+            u -= t * v;
+            swap(u, v);
+        }
+        assert(m == 1);
+        return u;
+    }
+
+    bool operator==(modint rhs) const { return value == rhs.value; }
+
+    bool operator!=(modint rhs) const { return !(*this == rhs); }
+
+    friend modint operator+(modint lhs, modint rhs) { return lhs += rhs; }
+
+    friend modint operator-(modint lhs, modint rhs) { return lhs -= rhs; }
+
+    friend modint operator*(modint lhs, modint rhs) { return lhs *= rhs; }
+
+    friend modint operator/(modint lhs, modint rhs) { return lhs /= rhs; }
+};
+
+constexpr int mod = (int)1e9 + 7;
+using mint = modint<mod>;
+
+
+/******** User-defined Function *******/
+void solve(vll &x, int n) {
+	
+}
+
+/**************************************/
+
+
+/********** Main()  function **********/
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	file_input;
+	//file_output;
+	#endif
+
+	int tc;
+	tc = read(int);
+
+	while (tc--) {
+		int n = read(int);
+		vll x(n);
+		for (auto &i : x) {
+			cin >> i;
+		}
+		solve(x, n);
+	}
+	return 0;
+}
+/********  Main() Ends Here *************/
