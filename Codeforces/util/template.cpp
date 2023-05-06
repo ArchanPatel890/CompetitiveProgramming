756382d6554e7075125072bb7bda5dbc78e7508b
ArchanPatel890 <archan.patel890@gmail.com>
2023-05-05 23:19:32 -0400

    Format and add additional utils to template.

--- Codeforces/util/template.cpp
+++ Codeforces/util/template.cpp
@@ -1,8 +1,8 @@
 /*
-* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
-*       Example:-   $ g++ -std=c++11 c++Template.cpp
-*/
- 
+ * Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
+ *       Example:-   $ g++ -std=c++11 c++Template.cpp
+ */
+
 /********   All Required Header Files ********/
 
 // C
@@ -12,31 +12,10 @@
 #include <cctype>
 #include <cerrno>
 #include <cfloat>
-#include <ciso646>
 #include <climits>
-#include <clocale>
 #include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <cwchar>
-#include <cwctype>
-
-#if __cplusplus >= 201103L
-#include <ccomplex>
-#include <cfenv>
-#include <cinttypes>
-#include <cstdalign>
-#include <cstdbool>
-#include <cstdint>
-#include <ctgmath>
-#include <cuchar>
-#endif
 
 // C++
 #include <algorithm>
@@ -74,59 +53,34 @@
 
 #if __cplusplus >= 201103L
 #include <array>
-#include <atomic>
-#include <chrono>
-#include <codecvt>
-#include <condition_variable>
-#include <forward_list>
-#include <future>
-#include <initializer_list>
 #include <mutex>
 #include <random>
 #include <ratio>
 #include <regex>
-#include <scoped_allocator>
-#include <system_error>
-#include <thread>
-#include <tuple>
-#include <typeindex>
-#include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
-#endif
-
-#if __cplusplus >= 201402L
-#include <shared_mutex>
-#endif
-
-#if __cplusplus >= 201703L
-#include <any>
-// #include <execution>
-#include <optional>
-#include <string_view>
-#include <variant>
 #endif
 
 using namespace std;
 
 /*******  All Required define Pre-Processors and typedef Constants *******/
 // IO
-#define SCD(t) scanf("%d",&t)
-#define SCLD(t) scanf("%ld",&t)
-#define SCLLD(t) scanf("%lld",&t)
-#define SCC(t) scanf("%c",&t)
-#define SCS(t) scanf("%s",t)
-#define SCF(t) scanf("%f",&t)
-#define SCLF(t) scanf("%lf",&t)
+#define SCD(t) scanf("%d", &t)
+#define SCLD(t) scanf("%ld", &t)
+#define SCLLD(t) scanf("%lld", &t)
+#define SCC(t) scanf("%c", &t)
+#define SCS(t) scanf("%s", t)
+#define SCF(t) scanf("%f", &t)
+#define SCLF(t) scanf("%lf", &t)
 #define read(type) readInt<type>()
 // Array / Iteration
 #define MEM(a, b) memset(a, (b), sizeof(a))
-#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
-#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define FOR(i, j, k, in) for (int i = j; i < k; i += in)
+#define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
 #define REP(i, j) FOR(i, 0, j, 1)
 #define RREP(i, j) RFOR(i, j, 0, 1)
 #define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define IN(A, B, C) assert( B <= A && A <= C)
+#define IN(A, B, C) assert(B <= A && A <= C)
 // Function Aliases
 #define all(cont) cont.begin(), cont.end()
 #define rall(cont) cont.end(), cont.begin()
@@ -140,7 +94,7 @@ using namespace std;
 #define EPS 1e-9
 #define PI 3.1415926535897932384626433832795
 const int MOD = 1000000007;
-const double pi=acos(-1.0);
+const double pi = acos(-1.0);
 // typedefs aliases for common types
 typedef long int int32;
 typedef unsigned long int uint32;
@@ -162,8 +116,8 @@ typedef vector<vc> vvc;
 typedef vector<vs> vvs;
 typedef vector<vpii> vvpii;
 typedef vector<vpll> vvpll;
-typedef map<int,int> mapii;
-typedef map<ll,ll> mapll;
+typedef map<int, int> mapii;
+typedef map<ll, ll> mapll;
 typedef map<string, string> mapss;
 typedef set<int> seti;
 typedef set<ll> setll;
@@ -176,21 +130,54 @@ typedef unordered_map<ll, ll> umapll;
 typedef unordered_map<string, string> umapss;
 // Timing
 clock_t start_time = clock();
-#define current_time fixed<<setprecision(6)<<(ld)(clock()-start_time)/CLOCKS_PER_SEC
+#define current_time fixed << setprecision(6) << (ld)(clock() - start_time) / CLOCKS_PER_SEC
 
 /****** Input / Output Redirection to Files ******/
-#define file_input freopen("input.txt","r",stdin)
-#define file_output freopen("output.txt","w",stdout)
-#define file_io file_input; file_output
-#define fast_io ios_base::sync_with_stdio(0);cin.tie(0)
+#define file_input freopen("input.txt", "r", stdin)
+#define file_output freopen("output.txt", "w", stdout)
+#define file_io \
+  file_input;   \
+  file_output
+#define fast_io                 \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0)
+/*************************************************/
 
 /****** Template of some basic operations *****/
-template<typename T, typename U> inline void amin(T &x, U y) { if (y < x) x = y; }
-template<typename T, typename U> inline void amax(T &x, U y) { if (x < y) x = y; }
+template <typename T, typename U>
+inline void amin(T &x, U y)
+{
+  if (y < x)
+    x = y;
+}
+template <typename T, typename U>
+inline void amax(T &x, U y)
+{
+  if (x < y)
+    x = y;
+}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+bool prime(ll a)
+{
+  if (a == 1)
+    return 0;
+  if (a == 2)
+    return 1;
+  if (a % 2 == 0)
+    return 0;
+  for (int i = 3; i <= round(sqrt(a)); i += 2)
+    if (a % i == 0)
+      return 0;
+  return 1;
+}
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
 /**********************************************/
 
 /****** Template of Fast I/O Methods *********/
-template <typename T> inline void write(T x)
+template <typename T>
+inline void write(T x)
 {
   int i = 20;
   char buf[21];
@@ -201,58 +188,64 @@ template <typename T> inline void write(T x)
   {
     buf[--i] = x % 10 + '0';
     x /= 10;
-  } while(x);
+  } while (x);
   do
   {
     putchar(buf[i]);
   } while (buf[i++] != '\n');
 }
 
-template <typename T> inline T readInt()
+template <typename T>
+inline T readInt()
 {
   T n = 0, s = 1;
   char p = getchar();
   if (p == '-')
     s = -1;
-  while ((p < '0' || p > '9') && p !=EOF && p != '-')
+  while ((p < '0' || p > '9') && p != EOF && p != '-')
     p = getchar();
   if (p == '-')
-    s = -1 , p = getchar();
-  while (p >= '0' && p <= '9') {
+    s = -1, p = getchar();
+  while (p >= '0' && p <= '9')
+  {
     n = (n << 3) + (n << 1) + (p - '0');
     p = getchar();
   }
 
-  return n*s;
+  return n * s;
 }
 /************************************/
 
-
 /***************** Debugging ******************/
-#define debug(x) cerr << #x << "=" << (x) <<'\n'
+#ifndef ONLINE_JUDGE
+#define dbg(v)                                                                 \
+  cerr << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#else
+#define dbg(v)
+#endif
 /**********************************************/
 
-
 /******** User-defined Function *******/
-void solve() {
-
+void solve()
+{
+  
 }
 
 /**************************************/
 
-
 /********** Main()  function **********/
 int main()
 {
-  #ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
   file_input;
   file_output;
-  #endif
+#endif
 
   int tc;
   tc = read(int);
 
-  while (tc--) {
+  while (tc--)
+  {
     
     solve();
   }
